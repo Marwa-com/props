8eb430fd2318422966edd4def620b7cddbcfe3a4
Marwa <amri.maroia@yahoo.fr>
2021-05-05 23:52:25 +0100

    adding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import myWonderfulImage from "./myImage.jpg";
 import './App.css';
+import MyProfile from './Profile/profile';
 
 function App() {
+  let fn ="Amri Marwa"
+  const handleName = () => alert(fn)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{minHeight:"100vh" ,backgroundColor:"lightgreen", textAlign:"center"}}>
+ <MyProfile fullname="Amri Marwa" bio="painting" profession="engineer" handleName={handleName}>
+<img  style={{width:200 ,height:200 ,borderRadius:100 }} src={myWonderfulImage} alt ='myImage'/>
+ </MyProfile>
     </div>
   );
 }
---
+++ src/Profile/profile.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const MyProfile = ( props ) =>{ 
+    console.log(props)
+    return(
+        <div>
+           <button style={{color:"black", fontWeight:"700"  ,backgroundColor:"blue" ,borderStyle:" 2 solid black" }} onClick={props.handleName}>
+                display username
+           </button> 
+           <h1  style={{color:"brown" ,fontStyle:"italic" ,fontWeight:"200" ,marginLeft:"200" }}>
+            hello, I'm {props.fullname}.I love {props.bio} and i'm an {props.profession} 
+           </h1>;
+            {props.children}
+        </div>
+ )
+}
+export default MyProfile;
+
+MyProfile.propTypes ={
+    fullname:PropTypes.string,
+    bio:PropTypes.string,
+    profession:PropTypes.string
+   
+}
+MyProfile.defaultProps = {
+    fullname:"username ",
+    bio:"interest",
+    profession: "job"
+   };

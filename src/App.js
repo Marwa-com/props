1fc0952b9e63b12a281bc1c4a875987eb87b23cb
Marwa <amri.maroia@yahoo.fr>
2021-05-06 01:29:00 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import myWonderfulImage from "./myImage.jpg";
+import myTabf from "./tab1.jpg";
+import myTabth from "./tab3.jpg";
 import './App.css';
 import MyProfile from './Profile/profile';
 
@@ -7,9 +9,17 @@ function App() {
   const handleName = () => alert(fn)
   return (
     <div className="App" style={{minHeight:"100vh" ,backgroundColor:"lightgreen", textAlign:"center"}}>
- <MyProfile fullname="Amri Marwa" bio="painting" profession="engineer" handleName={handleName}>
+ <MyProfile fullname="Amri Marwa" bio="painting" profession=" electrical engineer and developer full stack" handleName={handleName}>
 <img  style={{width:200 ,height:200 ,borderRadius:100 }} src={myWonderfulImage} alt ='myImage'/>
  </MyProfile>
+ <div >
+ <p style={{color:"black" ,fontStyle:"italic" , fontWeight:"400"}}> I code beautifully simple things, and I love what I do.<br/>you can contact
+   me with may <br/>Email:<span style={{color:"blue"}}> amri.maroia@yahoo.fr </span> <br/> In link below you can see some 
+   examples of my codes <br/> <span style={{color:"blue"}}> https://github.com/Marwa-com </span> </p> 
+<p style={{color:"black"}}> Also, i have some decorative paintings :  </p> <br/>
+<img  style={{width:200 ,height:200 ,borderRadius:20 }} src={myTabf} alt ='tab1'/>
+<img  style={{width:200 ,height:200 ,borderRadius:20  }} src={myTabth} alt ='tab3'/>
+ </div>
     </div>
   );
 }
--- src/Profile/profile.js
+++ src/Profile/profile.js
@@ -8,7 +8,7 @@ const MyProfile = ( props ) =>{
            <button style={{color:"black", fontWeight:"700"  ,backgroundColor:"blue" ,borderStyle:" 2 solid black" }} onClick={props.handleName}>
                 display username
            </button> 
-           <h1  style={{color:"brown" ,fontStyle:"italic" ,fontWeight:"200" ,marginLeft:"200" }}>
+           <h1  style={{color:"crimson" ,fontStyle:"italic" ,fontWeight:"200" ,marginLeft:"200" }}>
             hello, I'm {props.fullname}.I love {props.bio} and i'm an {props.profession} 
            </h1>;
             {props.children}
